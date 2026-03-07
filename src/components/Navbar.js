062051c8fe6680824711ca9aa87fc3d44c4c33da
Pinak <davepinak0@gmail.com>
2026-03-07 12:16:23 +0530

    update project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -172,7 +172,7 @@ const navbarStyles = `
 /* 🔥 EXTRA SMALL DEVICES (like 360px Androids / older iPhones) */
 @media (max-width: 400px) {
   .nav-container { padding: 0 12px; gap: 14px; }
-  .brand-logo { font-size: 1.6rem; margin-right: 9px  }
+  .brand-logo { font-size: 1.7rem; margin-right: 4px  }
   .nav-btn { padding: 8px 12px; font-size: 0.85rem; max-width: 140px; }
 }
 `;
